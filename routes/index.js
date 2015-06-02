53e16a2798fcafff3c551e729acca79b037e18fe
isasisipg <isasisipg@gmail.com>
2015-06-02 21:31:05 +0200

    Commit 6: Ejercicio módulo 6.
    Commit 6: Ejercicio módulo 6.

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get('/', function(req, res) {
 });
 
 router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer)
+router.get('/quizes/answer', quizController.answer);
+router.get('/author', function(req, res){
+	res.render('author')
+});
 
 module.exports = router;
