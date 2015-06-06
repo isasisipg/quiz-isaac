d178d7969a200586514291f993eebb3791afd0e7
isasisipg <isasisipg@gmail.com>
2015-06-06 18:09:18 +0200

    Revert "Commit 8: Múltiples preguntas"
    This reverts commit 63df9bf6e0037bc36a9a6ffe914be8fda745c831.

--- models/models.js
+++ models/models.js
@@ -37,8 +37,7 @@ sequelize.sync().success(function() {
 	Quiz.count().success(function (count){
 		if (count === 0) { // la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma'});
-			Quiz.create({ pregunta: 'Capital de Portugal', respuesta: 'Lisboa'});
-			Quiz.count().then(function(){console.log('Base de datos inicializada')});
+			Quiz.count().success(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
