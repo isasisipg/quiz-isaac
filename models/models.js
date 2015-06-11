df55ada2441ae9ca78b7ff1e80f3113840deafd1
isasisipg <isasisipg@gmail.com>
2015-06-11 22:02:34 +0200

    Commit 16: Crear Comentario.
    Commit 16: Crear Comentario.

--- models/models.js
+++ models/models.js
@@ -29,7 +29,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+// Importar la definición de la tabla Comment
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+// Relación entre las tablas
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() crea e inicializa tabla de preguntas en BD
 sequelize.sync().then(function() {
